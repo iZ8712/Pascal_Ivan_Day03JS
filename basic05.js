0375e6ab15d4086b8a0a15a40351cc9a3401d9de
Ivan Zivanovic <i.zivanovic01@gmail.com>
2020-09-22 11:43:09 +0200

    ex05

---
+++ basic05.js
@@ -0,0 +1,10 @@
+function box (width, height, depth) {
+        
+        var area = width * height;
+        var volum = width * height * depth;
+                
+        document.write("The area of the box is: " + area + "<br> The volume of the box is: " + volum);
+    
+}
+
+box(2,7,5);
