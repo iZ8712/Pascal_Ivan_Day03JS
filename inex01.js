031a578a8dac3c35484cbae2b5411e7b3fb0c532
Ivan Zivanovic <i.zivanovic01@gmail.com>
2020-09-22 13:05:44 +0200

    inex06

---
+++ inex01.js
@@ -0,0 +1,20 @@
+
+/*var x = function (a) {
+    
+    return a.charAt(0).toUpperCase() + a.slice(1);
+    
+}
+
+x("i am a web developer");*/
+
+
+
+
+var x = function(a) {
+    
+    return a.charAt(0).toUpperCase() + a.slice(1);
+
+};
+
+
+console.log(x("i am a web developer"));
